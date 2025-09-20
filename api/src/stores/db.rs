2b23621d3db251a17165727d53df2b9d882e7052
Guille <glexposito@gmail.com>
2025-09-21 11:27:30 +1200

    refactor: rename parameter from 'config' to 'settings' in make_ddb_client function

--- api/src/stores/db.rs
+++ api/src/stores/db.rs
@@ -2,11 +2,11 @@ use crate::models::app_settings::AppSettings;
 use aws_config;
 use aws_sdk_dynamodb::Client;
 
-pub async fn make_ddb_client(config: &AppSettings) -> Client {
+pub async fn make_ddb_client(settings: &AppSettings) -> Client {
     let mut loader = aws_config::defaults(aws_config::BehaviorVersion::latest());
 
-    if !config.dynamodb_endpoint.is_empty() {
-        loader = loader.endpoint_url(&config.dynamodb_endpoint);
+    if !settings.dynamodb_endpoint.is_empty() {
+        loader = loader.endpoint_url(&settings.dynamodb_endpoint);
     }
 
     let aws_config = loader.load().await;
